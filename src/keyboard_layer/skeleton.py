26e20a61af4f234f3d099150004c051c9fdbce2f
Noah Lekas <noah@noahlekas.com>
2023-07-11 19:49:58 -0400

    made keyboard layer shower

--- src/keyboard_layer/skeleton.py
+++ src/keyboard_layer/skeleton.py
@@ -21,46 +21,40 @@ References:
 """
 
 import argparse
-import logging
+from pathlib import Path
+
+from loguru import logger
 import sys
+import cv2
+import appdirs
+from pkg_resources import resource_filename
 
 from keyboard_layer import __version__
+
+from PIL import Image
 
 __author__ = "Noah Lekas"
 __copyright__ = "Noah Lekas"
 __license__ = "MIT"
 
-_logger = logging.getLogger(__name__)
 
-
-# ---- Python API ----
-# The functions defined in this section can be imported by users in their
-# Python scripts/interactive interpreter, e.g. via
-# `from keyboard_layer.skeleton import fib`,
-# when using this Python module as a library.
-
-
-def fib(n):
-    """Fibonacci example function
-
-    Args:
-      n (int): integer
-
-    Returns:
-      int: n-th Fibonacci number
-    """
-    assert n > 0
-    a, b = 1, 1
-    for _i in range(n - 1):
-        a, b = b, a + b
-    return a
-
-
-# ---- CLI ----
-# The functions defined in this section are wrappers around the main Python
-# API allowing them to be called directly from the terminal as a CLI
-# executable/script.
-
+def what_layer(n):
+    layer_images = {
+        0: resource_filename(__name__, "layer_diagrams/series_1/a9e_series_1_layer_0_pycharm.png"),
+        1: resource_filename(__name__,
+                                  "layer_diagrams/series_1/a9e_series1_layer_0_f_keys.png"),
+        2: resource_filename(__name__,
+                                  "layer_diagrams/series_1/a9e_keyboard_layout_layer_2_num_pad.png"),
+        9: resource_filename(__name__,
+                                  "layer_diagrams/series_1/a9e_keyboard_layout_layer_9_rgb_keys.png"),
+    }
+    image = cv2.imread(layer_images[n.layer])
+    print(layer_images[n.layer])
+    while True:
+        cv2.imshow(f"Layer {n.layer}", image)
+        cv2.waitKey(0)
+        break
+    cv2.destroyAllWindows()
 
 def parse_args(args):
     """Parse command line parameters
@@ -72,78 +66,42 @@ def parse_args(args):
     Returns:
       :obj:`argparse.Namespace`: command line parameters namespace
     """
-    parser = argparse.ArgumentParser(description="Just a Fibonacci demonstration")
-    parser.add_argument(
-        "--version",
-        action="version",
-        version=f"keyboard_layer {__version__}",
-    )
-    parser.add_argument(dest="n", help="n-th Fibonacci number", type=int, metavar="INT")
-    parser.add_argument(
-        "-v",
-        "--verbose",
-        dest="loglevel",
-        help="set loglevel to INFO",
-        action="store_const",
-        const=logging.INFO,
+    parser = argparse.ArgumentParser(
+        description="Shows layer diagram for the A9E keyboard"
     )
     parser.add_argument(
-        "-vv",
-        "--very-verbose",
-        dest="loglevel",
-        help="set loglevel to DEBUG",
-        action="store_const",
-        const=logging.DEBUG,
+        "-l",
+        "--layer",
+        help="The layer number diagram you want to see.",
+        type=int
     )
+    parser.set_defaults(func=what_layer)
+
     return parser.parse_args(args)
 
 
-def setup_logging(loglevel):
-    """Setup basic logging
-
-    Args:
-      loglevel (int): minimum loglevel for emitting messages
-    """
-    logformat = "[%(asctime)s] %(levelname)s:%(name)s:%(message)s"
-    logging.basicConfig(
-        level=loglevel, stream=sys.stdout, format=logformat, datefmt="%Y-%m-%d %H:%M:%S"
+def add_logging(src: str, log_path: Path) -> None:
+    logger.add(
+        log_path.joinpath(f"{src}.log"),
+        rotation="1 day",
+        retention="10 weeks",
+        compression="zip",
     )
 
 
-def main(args):
-    """Wrapper allowing :func:`fib` to be called with string arguments in a CLI fashion
-
-    Instead of returning the value from :func:`fib`, it prints the result to the
-    ``stdout`` in a nicely formatted message.
-
-    Args:
-      args (List[str]): command line parameters as list of strings
-          (for example  ``["--verbose", "42"]``).
-    """
-    args = parse_args(args)
-    setup_logging(args.loglevel)
-    _logger.debug("Starting crazy calculations...")
-    print(f"The {args.n}-th Fibonacci number is {fib(args.n)}")
-    _logger.info("Script ends here")
+def main(args) -> None:
+    source = args[1]
+    log_path = appdirs.user_data_dir(appname="A9E Layout Viewer", appauthor="Noah Lekas", version="0.1", roaming=True)
+    add_logging(src=source, log_path=Path(log_path))
+    logger.info("start")
+    args = parse_args(args[1:])
+    args.func(args)
+    logger.info("end")
 
 
 def run():
-    """Calls :func:`main` passing the CLI arguments extracted from :obj:`sys.argv`
-
-    This function can be used as entry point to create console scripts with setuptools.
-    """
-    main(sys.argv[1:])
+    main(sys.argv)
 
 
 if __name__ == "__main__":
-    # ^  This is a guard statement that will prevent the following code from
-    #    being executed in the case someone imports this file instead of
-    #    executing it as a script.
-    #    https://docs.python.org/3/library/__main__.html
-
-    # After installing your project with pip, users can also run your Python
-    # modules as scripts via the ``-m`` flag, as defined in PEP 338::
-    #
-    #     python -m keyboard_layer.skeleton 42
-    #
     run()
